3eaa1de896f04138d55ffc3d46c201f532d860ae
mmmurka <murenkodan2005@gmail.com>
2024-02-07 20:11:53 +0200

    weather

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
 import flet as ft
-
+import requests
 
 def main(page: ft.Page):
-    page.title = 'Flet App'
+    page.title = 'Погода'
     page.theme_mode = 'dark'
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    user_label = ft.Text('Info', color='#fafafa')
-    user_text = ft.TextField(value='0', width=150, text_align=ft.TextAlign.CENTER)
+    user_data = ft.TextField(label="Введите город", width=400)
+    weather_data = ft.Text('')
 
     def get_info(event):
-        user_label.value = user_text.value
+        if len(user_data.value) < 2:
+            return
+
+        API = 'f6af234f8ad6716fae9b66dcaeefdd36'
+        URL = f'https://api.openweathermap.org/data/2.5/weather?q={user_data.value}&APPID={API}&units=metric'
+        result = requests.get(URL).json()
+        try:
+            temp = result['main']['temp']
+            weather_data.value = f'Погода: {temp}°'
+        except:
+            print('error')
+        page.update()
+
+    def change_theme(event):
+        page.theme_mode = 'light' if page.theme_mode == 'dark' else 'dark'
         page.update()
 
     page.add(
         ft.Row(
             [
-                ft.IconButton(ft.icons.HOME, on_click=get_info),
-                ft.Icon(ft.icons.BACK_HAND),
-                ft.ElevatedButton(text='Click me', on_click=get_info),
-                ft.OutlinedButton(text='Click me', on_click=get_info),
-                ft.Checkbox(label='Вы согласны?', value=True)
+                ft.IconButton(ft.icons.SUNNY, on_click=change_theme),
+                ft.Text('Погодная программа')
             ],
             alignment=ft.MainAxisAlignment.CENTER
         ),
-        ft.Row(
-            [
-                user_label,
-                user_text
-            ],
-            alignment=ft.MainAxisAlignment.CENTER
-        ),
+        ft.Row([user_data], alignment=ft.MainAxisAlignment.CENTER),
+        ft.Row([weather_data], alignment=ft.MainAxisAlignment.CENTER),
+        ft.Row([ft.ElevatedButton(text='Получить', on_click=get_info)], alignment=ft.MainAxisAlignment.CENTER)
     )
+
 
 
 ft.app(target=main)
